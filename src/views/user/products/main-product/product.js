d7355d0f1208fd43f438c5dd072fd6fbff6ac5df
siha <es2008e@naver.com>
2022-11-03 10:41:51 +0900

    Feat : product itmes 뿌리기

---
+++ src/views/user/products/main-product/product.js
@@ -0,0 +1,28 @@
+import renderProducts from "./renderProducts.js";
+
+let contentEl = document.getElementById("input-products");
+
+async function handleData() {
+  await fetch("../../tempData.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const temp = data;
+      console.log(temp);
+      for (let i = 0; i < temp.length; i++) {
+        const [img, name, price] = [
+          temp[i].imgTitle,
+          temp[i].name,
+          temp[i].price,
+        ];
+        const htmlStr = renderProducts(img, name, price);
+        let el = document.createElement("div");
+        el.classList.add("column");
+        el.innerHTML = htmlStr;
+        contentEl.append(el);
+      }
+    });
+}
+
+window.onload = function () {
+  handleData();
+};
