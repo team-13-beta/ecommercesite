6f2d054dfe0d3b0e2c0f8c50bcef7dfc4a64b944
siha <es2008e@naver.com>
2022-11-07 16:37:26 +0900

    Feat : order user form

--- src/views/user/order/sendOrderData.js
+++ src/views/user/order/sendOrderData.js
@@ -2,7 +2,7 @@ const sendOrderEl = document.querySelector("#send-order");
 
 function handleSend() {
   // TODO : 주문 생성 post
-  console.log("click");
+
   window.location.href = "/user/order/success";
 }
 sendOrderEl.addEventListener("click", handleSend);
