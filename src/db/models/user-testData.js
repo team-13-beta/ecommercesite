9666476e7a93a08d9cc55bc6217dbca20f9dc731
siha <es2008e@naver.com>
2022-11-10 09:43:33 +0900

    Fix : conflict

--- src/db/models/user-testData.js
+++ src/db/models/user-testData.js
@@ -1,17 +1,13 @@
-import {userModel} from './user-model.js';
-import bcypt from 'bcrypt';
-import {timeZone} from '../../services/timeZone.js';
-import { userService } from '../../services/user-service.js';
-async function userModelTest(){
+import { userModel } from "./user-model.js";
+import bcypt from "bcrypt";
+import { timeZone } from "../../services/timeZone.js";
+import { userService } from "../../services/user-service.js";
+async function userModelTest() {
+  const pw = await bcypt.hash("1111", 10);
 
-const pw = await bcypt.hash('1111',10);
+  const time = timeZone();
 
-
-const time = timeZone();
-
-
-
-/*
+  /*
  async addUser(userInfo) {
     // 객체 destructuring
     const { email, fullName:name, password, phoneNumber, address } = userInfo;
@@ -38,109 +34,74 @@ const time = timeZone();
 
 */
 
-if(!await userModel.findByEmail("admin@example.com"))
-// admin은 role:admin 예외 지정이 필요, userModel로 create!
+  if (!(await userModel.findByEmail("admin@example.com")))
+    // admin은 role:admin 예외 지정이 필요, userModel로 create!
     await userModel.create({
-        userId:1,
-        email:"admin@example.com",
-        name:"admin",
-        password:String(pw),
-        phoneNumber:'010-2323-2424',
-        address:{  
-            postalCode: "000-000",
-            address1: "ex_City",
-            address2: "ex_apartMent"
-        },
-        role:'admin',
-        createdTime:time,
-        updatedTime:time
+      userId: 1,
+      email: "admin@example.com",
+      name: "admin",
+      password: String(pw),
+      phoneNumber: "010-2323-2424",
+      address: {
+        postalCode: "000-000",
+        address1: "ex_City",
+        address2: "ex_apartMent",
+      },
+      role: "admin",
+      createdTime: time,
+      updatedTime: time,
     });
-   
-if(!await userModel.findByEmail("abc@example.com"))
+
+  if (!(await userModel.findByEmail("abc@example.com")))
     await userService.addUser({
-        email:"abc@example.com",
-        fullName:"가가",
-        password:"1111",
-        phoneNumber:'010-2323-2424',
-        address:{  
-            postalCode: "000-000",
-            address1: "ex_City",
-            address2: "ex_apartMent"
-        },
+      email: "abc@example.com",
+      fullName: "가가",
+      password: "1111",
+      phoneNumber: "010-2323-2424",
+      address: {
+        postalCode: "000-000",
+        address1: "ex_City",
+        address2: "ex_apartMent",
+      },
     });
 
-if(!await userModel.findByEmail("aaa@example.com"))
+  if (!(await userModel.findByEmail("aaa@example.com")))
     await userService.addUser({
-        email:"aaa@example.com",
-        fullName:"나나",
-        password:"1111",
-        phoneNumber:'010-2323-2424',
-        address:{  
-            postalCode: "000-000",
-            address1: "ex_City",
-            address2: "ex_apartMent"
-        },
+      email: "aaa@example.com",
+      fullName: "나나",
+      password: "1111",
+      phoneNumber: "010-2323-2424",
+      address: {
+        postalCode: "000-000",
+        address1: "ex_City",
+        address2: "ex_apartMent",
+      },
+    });
 
-    })
-
-if(!await userModel.findByEmail("bbb@example.com"))
+  if (!(await userModel.findByEmail("bbb@example.com")))
     await userService.addUser({
-        email:"bbb@example.com",
-        fullName:"다다",
-        password:"1111",
-        phoneNumber:'010-2323-2424',
-        address:{  
-            postalCode: "000-000",
-            address1: "ex_City",
-            address2: "ex_apartMent"
-        }
-    });
-
-if(!await userModel.findByEmail("ccc@example.com"))
-   await userService.addUser({
-        email:"ccc@example.com",
-        fullName:"라라",
-        password:"1111",
-        phoneNumber:'010-2323-2424',
-        address:{  
-            postalCode: "000-000",
-            address1: "ex_City",
-            address2: "ex_apartMent"
-        }
-    });
-}
-export {userModelTest};
-
-  if (!(await userModel.findByEmail("bbb@example.com"))) {
-    await userModel.create({
-      userId: 4,
       email: "bbb@example.com",
-      name: "다다",
-      password: pw,
+      fullName: "다다",
+      password: "1111",
       phoneNumber: "010-2323-2424",
       address: {
         postalCode: "000-000",
         address1: "ex_City",
         address2: "ex_apartMent",
       },
-      createdTime: time,
-      updatedTime: time,
     });
-  }
+
   if (!(await userModel.findByEmail("ccc@example.com")))
-    await userModel.create({
-      userId: 5,
+    await userService.addUser({
       email: "ccc@example.com",
-      name: "라라",
-      password: pw,
+      fullName: "라라",
+      password: "1111",
       phoneNumber: "010-2323-2424",
       address: {
         postalCode: "000-000",
         address1: "ex_City",
         address2: "ex_apartMent",
       },
-      createdTime: time,
-      updatedTime: time,
     });
 }
 export { userModelTest };
